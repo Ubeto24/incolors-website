e0c06a34d1f3eea2ad7eafea4e8f29c13ce26bf8
Carlos Barrios Perez <beto@MacBook-Air-de-Carlos.local>
2025-05-14 12:54:57 -0400

    fix: corregido ProductOptions.jsx

--- src/components/IncolorsSocialSection.jsx
+++ src/components/IncolorsSocialSection.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export default function IncolorsSocialSection() {
   return <div>Redes Sociales</div>;
 }
--- src/components/ProductOptions.jsx
+++ src/components/ProductOptions.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export default function ProductOptions() {
   return <div>Opciones de Producto</div>;
 }

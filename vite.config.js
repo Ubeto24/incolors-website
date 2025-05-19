fc4bc7919ae2cce731f79a8988d67e4d96f3c3ed
Carlos Barrios <carlosbarriosp03@gmail.com>
2025-05-19 14:32:30 -0400

    solucionv6

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['lucide-react'] // 🔧 Evita que rompa el bundle de producción
-    }
-  }
 });
